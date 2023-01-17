869bc2463eae452795049bd4e00ff53a4b7d3552
Daniel <angelicodaniel93@gmail.com>
2023-01-17 15:59:41 +0100

    seeder foreach

--- database/seeds/TrainsTableSeeder.php
+++ database/seeds/TrainsTableSeeder.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Train;
 use Illuminate\Database\Seeder;
 
 class TrainsTableSeeder extends Seeder
@@ -18,8 +19,21 @@ class TrainsTableSeeder extends Seeder
             'stazione_arrivo' => 'Bari',
             'orario_partenza' => '10:15:00',
             'orario_arrivo' => '16:00:00',
-            'prezzo' => '39,90',
+            'prezzo' => 39.90,
             'is_available' => false,
         ];
+
+        foreach ($trains as $elem) {
+            $newTrain = new Train();
+            $newTrain->codice_treno = $elem['codice_treno'];
+            $newTrain->n_carrozze = $elem['n_carrozze'];
+            $newTrain->stazione_partenza = $elem['stazione_partenza'];
+            $newTrain->stazione_arrivo = $elem['stazione_arrivo'];
+            $newTrain->orario_partenza = $elem['orario_partenza'];
+            $newTrain->orario_arrivo = $elem['orario_arrivo'];
+            $newTrain->prezzo = $elem['prezzo'];
+            $newTrain->is_available = $elem['is_available'];
+            $newTrain->save();
+        }
     }
 }
