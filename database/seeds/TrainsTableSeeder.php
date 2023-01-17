0be04adc3badde41bbd757506965121b67b615bc
Daniel <angelicodaniel93@gmail.com>
2023-01-17 17:06:18 +0100

    last commit

--- database/seeds/TrainsTableSeeder.php
+++ database/seeds/TrainsTableSeeder.php
@@ -12,16 +12,39 @@ class TrainsTableSeeder extends Seeder
      */
     public function run()
     {
+
         $trains = [
-            'codice_treno' => 'qwerty',
-            'n_carrozze' => 3,
-            'stazione_partenza' => 'Ferrara',
-            'stazione_arrivo' => 'Bari',
-            'orario_partenza' => '10:15:00',
-            'orario_arrivo' => '16:00:00',
-            'prezzo' => 39.90,
-            'is_available' => 1
-        ];
+            [
+                'codice_treno' => 'qwerty',
+                'n_carrozze' => 3,
+                'stazione_partenza' => 'Ferrara',
+                'stazione_arrivo' => 'Bari',
+                'orario_partenza' => '10:15:00',
+                'orario_arrivo' => '16:00:00',
+                'prezzo' => 39.90,
+                'is_available' => 1
+            ],
+            [
+                'codice_treno' => 'zxcvb',
+                'n_carrozze' => 9,
+                'stazione_partenza' => 'Napoli',
+                'stazione_arrivo' => 'Roma',
+                'orario_partenza' => '19:40:00',
+                'orario_arrivo' => '21:00:00',
+                'prezzo' => 19.90,
+                'is_available' => 1
+            ],
+            [
+                'codice_treno' => 'zxcvb',
+                'n_carrozze' => 9,
+                'stazione_partenza' => 'Foggia',
+                'stazione_arrivo' => 'Torino',
+                'orario_partenza' => '03:40:00',
+                'orario_arrivo' => '8:00:00',
+                'prezzo' => 666.00,
+                'is_available' => 0
+            ],
+    ];
 
         foreach ($trains as $elem) {
             $newTrain = new Train();
--- resources/views/pages/welcome.blade.php
+++ resources/views/pages/welcome.blade.php
@@ -1,4 +1,32 @@
 @extends('layouts.app')
 @section('main-content')
-    <h1>I TRENI</h1>
+<table class="table">
+    <thead>
+      <tr>
+        <th scope="col">Codice Treno</th>
+        <th scope="col">Carrozze</th>
+        <th scope="col">Stazione Partenza</th>
+        <th scope="col">Stazione Arrivo</th>
+        <th scope="col">Orario Partenza</th>
+        <th scope="col">Orario Arrivo</th>
+        <th scope="col">Prezzo</th>
+        <th scope="col">Disponibilità</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        @foreach ($trains as $elem)
+            <td>{{ $elem->codice_treno}}</td>
+            <td>{{ $elem->n_carrozze}}</td>
+            <td>{{ $elem->stazione_partenza}}</td>
+            <td>{{ $elem->stazione_arrivo}}</td>
+            <td>{{ $elem->orario_partenza}}</td>
+            <td>{{ $elem->orario_arrivo}}</td>
+            <td>{{ $elem->prezzo}}</td>
+            <td>{{ $elem->is_available}}</td>
+        @endforeach
+    </tr>
+
+    </tbody>
+  </table>
 @endsection
